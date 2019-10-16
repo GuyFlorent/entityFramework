83a90d8a582506bf967585de8d003edf78bfc28e
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-10-16 14:45:31 +0200

    création de la d'une instance de la base de donnée et ensuite les requetes

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace entityFramework
     {
         static void Main(string[] args)
         {
+            NorthwindEntities dbContext = new NorthwindEntities();
+           var a = dbContext.Customers.ToList();
+
+            var custom = a.Where(f => f.Country == "France");
+
+            foreach(var b in custom)
+            {
+               // Console.WriteLine(b.ContactName);
+            }
+
+            var person = dbContext.Customers.FirstOrDefault(f => f.ContactName == "Maria Anders");
+            
+            
+                
+
+            var l = dbContext.Orders.Where(f => f.CustomerID == person.CustomerID);
+
+            foreach (var c in l)
+            {
+                Console.WriteLine(c.OrderID+" "+c.ShippedDate);
+            }
         }
     }
 }
